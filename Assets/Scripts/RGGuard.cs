cf8e665646704b32f6b4e9db09e7c954c224bd57
Robert Utter <utter.robert@gmail.com>
2013-05-30 13:11:52 -0700

    fleshing out guard a bit

--- Assets/Scripts/RGGuard.cs
+++ Assets/Scripts/RGGuard.cs
@@ -7,13 +7,91 @@ public class RGGuard : MonoBehaviour {
 	
 	CharacterController controller;
 	
+	Collider platform;
+	
+	const float SHOT_DELAY = 0.2f;
+	
+	const float MOVE_RATE = 1f;
+	
+	float timeToNextShot;
+	
+	Vector3 startPos;
+	Vector3 endPos;
+	
+	Vector3 minPos;
+	Vector3 maxPos;
+	
+	bool bMoving;
+	
 	void Start()
 	{
 		controller = GetComponent<CharacterController>();
+		
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, Vector3.down, out hit, 100f)) {
+			platform = hit.collider;
+			
+			Vector3 newPos = hit.point;
+			newPos.y = hit.collider.bounds.max.y + collider.bounds.size.y * 0.5f;
+			transform.position = newPos;
+		} else {
+			Debug.Log("couldn't find floor: suiciding");
+			Destroy(gameObject);
+		}
+		
+		//TODO
+		platform = collider;
+		
+		Bounds bounds = platform.bounds;
+		minPos = new Vector3(bounds.min.x, bounds.max.y + collider.bounds.size.y * 0.5f, bounds.center.z);
+		maxPos = new Vector3(bounds.max.x, bounds.max.y + collider.bounds.size.y * 0.5f, bounds.center.z);
 	}
 	
 	void Update()
 	{
+		if (CanSeePlayer()) {
+			timeToNextShot -= Time.deltaTime;
+			if (timeToNextShot < 0f) {
+				Shoot();
+				timeToNextShot = SHOT_DELAY;
+			}
+		} else {
+			timeToNextShot = 0f;
+			
+			if (bMoving) {
+				//TODO
+			}
+		}
+	}
+	
+	void StartMoving()
+	{
+		startPos = transform.position;
+		endPos = Vector3.Lerp(minPos, maxPos, Random.value);
 		
+		transform.LookAt(endPos);
+		
+		bMoving = true;
+	}
+	
+	void StopMoving()
+	{
+		float param = Random.Range(Mathf.PI * 0.1f, Mathf.PI * 0.5f);
+		float delay = Mathf.Sin(param);
+		
+		Invoke("StartMoving", delay);
+		
+		bMoving = false;
+	}
+	
+	void Shoot()
+	{
+		//TODO
+	}
+	
+	bool CanSeePlayer()
+	{
+		return Vector3.Angle(transform.forward, RGPlayer.instance.transform.position - transform.position) < 30f;
+		//TODO
 	}
 }
