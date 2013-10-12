9a435296a6b7b7bc8cffdfda187209300bb7eec9
Robert Utter <utter.robert@gmail.com>
2013-10-11 17:58:32 -0700

    GameObjectExtensions

---
+++ Assets/Plugins/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameObjectExtensions {
+	public static void SetLayerRecursively(this GameObject root, int layer) {
+		root.layer = layer;
+		foreach (Transform child in root.transform) {
+			child.gameObject.SetLayerRecursively(layer);
+		}
+	}
+	
+	public static void SetLayerRecursively(this GameObject root, string layerName) {
+		int layer = LayerMask.NameToLayer(layerName);
+		root.SetLayerRecursively(layer);
+	}
+}
