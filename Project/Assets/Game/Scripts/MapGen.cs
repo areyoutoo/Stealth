ca39c2bfdab8fc72b0b9ffa6b948f390d3247454
Robert Utter <utter.robert@gmail.com>
2013-10-13 18:26:16 -0700

    progress on IntVector2

--- Project/Assets/Game/Scripts/MapGen.cs
+++ Project/Assets/Game/Scripts/MapGen.cs
@@ -2,8 +2,27 @@
 using System.Collections.Generic;
 
 public class MapGen : MonoBehaviour {
-	
-	
 	RoomInfo currentRoom;
 	List<RoomInfo> rooms;
+	
+	Dictionary<IntVector2, RoomInfo> roomMap;
+	
+	[SerializeField] GameObject barrierPrefab;
+	[SerializeField] GameObject platformPrefab;
+	
+	protected void Awake() {
+		rooms = new List<RoomInfo>();
+		roomMap = new Dictionary<IntVector2, RoomInfo>();
+	}
+	
+	protected void Start() {
+	}
+	
+	void AddRoom(IntVector2 pos) {
+	}
+	
+	void ConnectRoom(IntVector2 pos, IntVector2 dir) {
+		if (dir.gridMagnitude > 1) {
+		}
+	}
 }
---
+++ Project/Assets/Plugins/Stealth/IntVector2.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct IntVector2 {
+	int x;
+	int y;
+	
+	public float magnitude {
+		get {
+			return Mathf.Sqrt(x*x + y*y);
+		}
+	}
+	
+	public int gridMagnitude {
+		get {
+			return Mathf.Abs(x) + Mathf.Abs(y);
+		}
+	}
+	
+	public IntVector2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public static IntVector2 north {
+		get {
+			return new IntVector2(0,1);
+		}
+	}
+	
+	public static IntVector2 south {
+		get {
+			return new IntVector2(0,-1);
+		}
+	}
+	
+	public static IntVector2 east {
+		get {
+			return new IntVector2(1,0);
+		}
+	}
+	
+	public static IntVector2 west {
+		get {
+			return new IntVector2(-1,0);
+		}
+	}
+	
+	public static IntVector2 operator +(IntVector2 a, IntVector2 b) {
+		return new IntVector2(a.x + b.x, a.y + b.y);
+	}
+	
+	public static IntVector2 operator -(IntVector2 a, IntVector2 b) {
+		return new IntVector2(a.x - b.x, a.y - b.y);
+	}
+	
+	public static IntVector2 operator -(IntVector2 a) {
+		return new IntVector2(-a.x, -a.y);
+	}
+	
+	public static IntVector2 operator *(IntVector2 a, IntVector2 b) {
+		return new IntVector2(a.x * b.x, a.y * b.y);
+	}
+	
+	public static IntVector2 operator *(IntVector2 a, int scale) {
+		return new IntVector2(a.x * scale, a.y * scale);
+	}
+}
