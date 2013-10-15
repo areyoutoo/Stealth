34379120940054062b47d820e654e7e6251c7e77
Robert Utter <utter.robert@gmail.com>
2013-10-15 00:32:03 -0700

    gaps that mostly work!

--- Project/Assets/Game/Scripts/MapGen.cs
+++ Project/Assets/Game/Scripts/MapGen.cs
@@ -9,6 +9,7 @@ public class MapGen : MonoBehaviour {
 	const float HALF_ROOM_SIZE = ROOM_SIZE / 2f;
 	const float BARRIER_WIDTH = 1f;
 	const float BARRIER_LENGTH = ROOM_SIZE + BARRIER_WIDTH;
+	const float GAP_LENGTH = 4f;
 	
 	Dictionary<IntVector2, RoomInfo> roomMap;
 	
@@ -47,15 +48,30 @@ public class MapGen : MonoBehaviour {
 		SpawnWall(center - Vector3.right * HALF_ROOM_SIZE);
 	}
 	
-	void SpawnCeiling(Vector3 center) {
-		GameObject ceiling = (GameObject)Instantiate(barrierPrefab);
-		ceiling.transform.localScale = new Vector3(BARRIER_LENGTH, BARRIER_WIDTH, BARRIER_WIDTH);
-		ceiling.transform.position = center;
+	void SpawnCeiling(Vector3 center) {		
+		float far_left = center.x - HALF_ROOM_SIZE - BARRIER_WIDTH;
+		float far_right = center.x + HALF_ROOM_SIZE + BARRIER_WIDTH;
+		
+		float gap_start = Random.Range(far_left + BARRIER_WIDTH, far_right - GAP_LENGTH - BARRIER_WIDTH);
+		float gap_end = gap_start + GAP_LENGTH;
+		
+		Vector3 left_center = new Vector3(Mathf.Lerp(far_left, gap_start, 0.5f), center.y, center.z);
+		Vector3 right_center = new Vector3(Mathf.Lerp(gap_end, far_right, 0.5f), center.y, center.z);
+		
+		Vector3 left_scale = new Vector3(gap_start - far_left, BARRIER_WIDTH, BARRIER_WIDTH);
+		Vector3 right_scale = new Vector3(far_right - gap_end, BARRIER_WIDTH, BARRIER_WIDTH);
+		
+		SpawnBlock(left_center, left_scale);
+		SpawnBlock(right_center, right_scale);
 	}
 	
 	void SpawnWall(Vector3 center) {
-		GameObject wall = (GameObject)Instantiate(barrierPrefab);
-		wall.transform.localScale = new Vector3(BARRIER_WIDTH, BARRIER_LENGTH, BARRIER_WIDTH);
-		wall.transform.position = center;
+		SpawnBlock(center, new Vector3(BARRIER_WIDTH, BARRIER_LENGTH, BARRIER_WIDTH));
+	}
+	
+	void SpawnBlock(Vector3 center, Vector3 scale) {
+		GameObject block = (GameObject)Instantiate(barrierPrefab);
+		block.transform.localScale = scale;
+		block.transform.position = center;
 	}
 }
